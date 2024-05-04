0088ab316a63fc8c78efac7e99746749b2cb222c
Roman Paymulin <romanpaumulin@gmail.com>
2024-05-04 13:35:42 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  // devtool: 'inline-source-map',
+  entry: './src/index.js', // точка входа в ваше приложение
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'PixiTempalte',
+      template: 'src/index.html'  // Путь к шаблону HTML, если необходим
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+    ]
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true
+  }
+};
